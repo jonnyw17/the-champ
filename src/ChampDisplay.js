53c4b4870323b20c54ff6bb91e58b6a5d07bc147
Jonny Ward <JonnyWard@Macs-MacBook-Pro.local>
2017-11-21 16:24:41 +0000

    [Platform] added minimal style

--- src/ChampDisplay.js
+++ src/ChampDisplay.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 class ChampDisplay extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentChamp: [
+        {
+          player: "Anthony"
+        }
+      ]
+    }
+  }
   render() {
     return (
-        <Link to="/battledisplay">
-          <h1>Champ Display</h1>
-        </Link>
+      <Link to="/battledisplay">
+        <h1>Champ Display</h1>
+      </Link>
     );
   }
 }
--- src/Platform.js
+++ src/Platform.js
@@ -2,22 +2,26 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 class Platform extends Component {
   render() {
-    return (<div>
+    return (
+    <div className="platForm-main-div">
       <Link to="/userform">
-        <div className="platform">
-          <h3>Worms</h3>
+        <div className="platform-top">
+          <h3 className="platform-name">Worms W.M.D.</h3>
         </div>
       </Link>
       <div className="platform">
-        <h3>League of Legends</h3>
+        <h3 className="platform-name">League of Legends</h3>
+        <p>coming soon</p>
       </div>
       <div className="platform">
-        <h3>Starcraft II</h3>
+        <h3 className="platform-name">Starcraft II</h3>
+        <p>coming soon</p>
       </div>
       <div className="platform">
-        <h3>Dota 2</h3>
+        <h3 className="platform-name">Dota 2</h3>
       </div>
-    </div>)
+    </div>
+    )
   }
 }
 
