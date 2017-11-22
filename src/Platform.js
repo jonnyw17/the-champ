e9bd870594bfd520e6d2cf5e178fe085956bc5e7
Jonny Ward <JonnyWard@Macs-MacBook-Pro.local>
2017-11-22 13:32:53 +0000

    [Platform] additional style added, needs placeholder images

--- src/Platform.js
+++ src/Platform.js
@@ -2,26 +2,33 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 class Platform extends Component {
   render() {
-    return (
-    <div className="platForm-main-div">
+    return (<div className="platForm-main-div">
+      <div className="platform-nav">
+        <p className="platform-header">Platform</p>
+      </div>
       <Link to="/userform">
-        <div className="platform-top">
-          <h3 className="platform-name">Worms W.M.D.</h3>
+        <div className="platform">
+            <h3 className="platform-name-top">Worms W.M.D.</h3>
         </div>
       </Link>
       <div className="platform">
-        <h3 className="platform-name">League of Legends</h3>
-        <p>coming soon</p>
+        <h3 className="platform-name">League of Legends<br/>
+          <span className="coming-soon">Coming Soon</span>
+        </h3>
       </div>
       <div className="platform">
-        <h3 className="platform-name">Starcraft II</h3>
-        <p>coming soon</p>
+        <h3 className="platform-name">Starcraft II
+          <br/>
+          <span className="coming-soon">Coming Soon</span>
+        </h3>
       </div>
       <div className="platform">
-        <h3 className="platform-name">Dota 2</h3>
+        <h3 className="platform-name">Dota 2
+          <br/>
+          <span className="coming-soon">Coming Soon</span>
+        </h3>
       </div>
-    </div>
-    )
+    </div>)
   }
 }
 
